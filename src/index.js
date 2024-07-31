fd8b4061c80a2f5c5260100d568f51b4884671b0
Rappykyun <rakuichijo155@gmail.com>
2024-07-31 19:34:12 +0800

    made a file structures of a webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './styles.css';
+import myImage from './1364818.jpeg';
+
+const img = document.createElement('img');
+img.src = myImage;
+document.body.appendChild(img);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
